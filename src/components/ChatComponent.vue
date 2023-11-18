66ee2dd52ae2809da094646969e4d9e820a62e61
徐志斌 <1262254123@qq.com>
2023-11-18 19:15:37 +0800

    QuickChat前端项目搭建

---
+++ src/components/ChatComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="border: 1px solid red;">
+    11111111111111
+    <br>
+    111111111111
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
