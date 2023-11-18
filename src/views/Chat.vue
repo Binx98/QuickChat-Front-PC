59a6ae770b5d87edd9f305ef4d8c0ad6173a88ed
徐志斌 <1262254123@qq.com>
2023-11-18 17:44:28 +0800

    QuickChat-Front

---
+++ src/views/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  111111111
+</template>
+
+<script>
+// import Chat from "@/components/Chat";
+
+export default {
+  name: "Chat",
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
