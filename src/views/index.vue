e72b1de77d301c306502a9ccef33833c9e06d157
徐志斌 <1262254123@qq.com>
2023-12-02 14:05:10 +0800

    QuickChat前端项目搭建

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,12 +3,12 @@
     <div style="background-color: #292A2D;width: 100%;height: 100%;border-radius: 16px;border: 1px solid yellow;">
       <el-row :gutter="12" style="border: 1px solid red;height: 100%">
         <!--  0.导航栏 -->
-        <el-col :span="4"
+        <el-col :span="5"
                 style="border: 1px solid red;height: 10%;display: flex; justify-content: center; align-items: center;">
           <el-image :src="require('@/assets/logo/logo2_transparent.png')"
                     style="width: 100%; height: 100%; object-fit: cover;"/>
         </el-col>
-        <el-col :span="16"
+        <el-col :span="15"
                 style="border: 1px solid red;height: 10%;display: flex; justify-content: center; align-items: center;">
           <el-input style="width: 50%"/>
         </el-col>
@@ -21,14 +21,23 @@
         </el-col>
 
         <!--  1.会话列表 -->
-        <el-col :span="4" style="border: 1px solid red;height: 90%;">
-          <div class="friend-item" style="background-color: #323335;border-radius: 6px;display: flex;margin-bottom: 2px">
+        <el-col :span="5" style="border: 1px solid red;height: 90%;">
+          <div class="friend-item"
+               style="background-color: #323335;border-radius: 6px;margin-bottom: 2px;display: flex;width: 100%;">
             <el-avatar shape="square" :size="55"/>
+            <div style="width: 40%">
+              <div style="border: 1px solid red;height: 45%;">昵称</div>
+              <div style="border: 1px solid red;height: 55%;">聊息</div>
+            </div>
+            <div style="width: 30%">
+              <div style="border: 1px solid red;height: 45%">信息</div>
+              <div style="border: 1px solid red;height: 55%;width: 60%">聊天</div>
+            </div>
           </div>
         </el-col>
 
         <!--  2、聊天窗口  -->
-        <el-col :span="16" style="background-color: #323335;border-radius: 10px;height: 90%;overflow: auto;">
+        <el-col :span="15" style="background-color: #323335;border-radius: 10px;height: 90%;overflow: auto;">
           <!--  2.1、上边栏  -->
           <div style="display: flex; align-items: center;padding-left: 1%;border: 1px solid red;height: 9%">
             <el-avatar :size="48" shape="square" style="margin-right: 1%;cursor:pointer;"/>
@@ -60,37 +69,9 @@
                 <el-avatar shape="square" style="cursor:pointer;"/>
               </span>
             </div>
-            <!--  主动发送  -->
-            <div
-                style="float: right;width: 70%;margin-right: 1%;margin-top:1%;display: flex; align-items: center;justify-content: flex-end">
-              <div
-                  style="margin-left: 1.6%;background-color: mediumspringgreen;border-radius: 10px;display: inline-block;">
-                <div style="padding: 15px;font-size: 14px;word-break: break-all">
-                  2222222222222222222222222222222222222222222222
-                  2222222222222222222222222222222222222222222222
-                  2222222222222222222222222222222222222222222222
-                </div>
-              </div>
-              <span style="margin-left: 6px">
-                <el-avatar shape="square" style="cursor:pointer;"/>
-              </span>
-            </div>
-            <!--  主动发送  -->
-            <div
-                style="float: right;width: 80%;margin-right: 1%;margin-top:1%;display: flex; align-items: center;justify-content: flex-end;">
-              <div
-                  style="margin-left: 1.6%;background-color: mediumspringgreen;border-radius: 10px;display: inline-block;">
-                <div style="padding: 15px;font-size: 14px;word-break: break-all">
-                  2222222222222222222222222222222222222222222222
-                </div>
-              </div>
-              <span style="margin-left: 6px">
-                <el-avatar shape="square" style="cursor:pointer;"/>
-              </span>
-            </div>
           </div>
           <!-- 输入框 -->
-          <div style="margin-top: 1%;border: 1px solid yellow;height: 8%">
+          <div style="margin-top: 1%;border: 1px solid yellow;height: 8%;">
             <el-input id="chat-input-id" placeholder="请开始你的表演......" type="text"/>
           </div>
         </el-col>
@@ -145,7 +126,7 @@ export default {
 
 <style scoped>
 .main {
-  width: 80%;
+  width: 78%;
   height: 88%;
   margin: auto;
   position: absolute;
@@ -164,11 +145,8 @@ export default {
 #chat-input-id {
   border-radius: 20px;
   font-size: 14px;
-  background-color: #323335;
-  color: white;
   white-space: pre-wrap;
   overflow-y: auto;
-  max-height: 40px;
   resize: vertical;
 }
 </style>
