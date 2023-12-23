c995ad500dd55abf3d309c93406b5cc134240494
徐志斌 <1262254123@qq.com>
2023-12-23 09:33:08 +0800

    QuickChat页面开发

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,19 @@
 <template>
   <div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #232427">
-    <div style="background-color: #1f2023; width: 50%; height: 80%;">
-      <el-input style="width: 20vw;display: block" placeholder="请输入账号"/>
-      <el-input style="width: 20vw;display: block" placeholder="请输入密码"/>
-      <el-input style="width: 20vw;display: block" placeholder="请输入验证码"/>
-      <el-button>登录</el-button>
+    <div style="width: 100%; height: 100%;border: 1px solid red;">
+<!--      &lt;!&ndash; 左上角LOGO &ndash;&gt;-->
+<!--      <div style="width: 10%; height: 10%;border: 1px solid yellow">-->
+<!--        <el-image :src="require('@/assets/logo/logo2_transparent.png')"-->
+<!--                  style=" object-fit: cover;"/>-->
+<!--      </div>-->
+
+      <!--   表单   -->
+      <div style="border: 1px solid red;width: 20vw;text-align: center;">
+        <el-input style="display: block;opacity: 0.4" placeholder="请输入账号"/>
+        <el-input style="display: block;opacity: 0.4" placeholder="请输入密码"/>
+        <el-input style="display: block;opacity: 0.4" placeholder="请输入验证码"/>
+        <el-button type="primary" size="medium" style="background-color: #12CEC2FF;width: 10vw">登录</el-button>
+      </div>
     </div>
   </div>
 </template>
