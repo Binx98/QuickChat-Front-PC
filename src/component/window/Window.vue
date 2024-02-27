c229f96548801f1e976ac591fdf9633e3229f435
徐志斌 <1262254123@qq.com>
2024-02-27 21:05:32 +0800

    feat：emoji表情功能开发

--- src/component/window/Window.vue
+++ src/component/window/Window.vue
@@ -1,14 +1,14 @@
 <template>
   <span>
-    <!--  1、选中会话  -->
+    <!--  选中会话  -->
     <el-col class="window-cls" v-if="this.curSession != ''">
-      <!--  上边栏  -->
+      <!--  1.上边栏  -->
       <div class="head-bar">
         <el-avatar :src="curSession.sessionAvatar" shape="square" style="margin-right: 1%;cursor:pointer"/>
         <div class="session-name-cls">{{ curSession.sessionName }}</div>
       </div>
 
-      <!--  窗口  -->
+      <!--  2.窗口  -->
       <div id="window-id" class="msg-window-cls">
         <div v-for="item in chatMsgList[curSession.relationId]">
           <!--  接收信息  -->
@@ -37,13 +37,54 @@
         </div>
       </div>
 
-      <!-- 输入框 -->
+      <!-- 3.输入部分 -->
       <div class="input-cls">
+        <!-- 语音 -->
         <span>语音、</span>
+
+        <!-- 输入框 -->
         <input @keyup.enter="sendMsg()" id="chat-input" placeholder="请开始你的表演......"
                v-model="chatMsg.content"/>
-        <Emoji/>
+
+        <!-- Emoji -->
+        <span class="wrapper">
+          <emoji-picker @emoji="">
+            <!-- 按钮 -->
+            <button
+                class="emoji-invoker"
+                slot="emoji-invoker"
+                slot-scope="{ events: { click: clickEvent } }"
+                @click.stop="clickEvent"
+            >
+               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-grey">
+                  <path d="M0 0h24v24H0z" fill="none"/>
+                  <path
+                      d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+               </svg>
+            </button>
+
+            <!-- 表情框 -->
+            <div slot="emoji-picker" slot-scope="{ emojis, insert, display }">
+              <div class="emoji-picker" :style="{ top: display.y + 'px', left: display.x + 'px' }">
+                <div v-for="(emojiGroup, category) in emojis" :key="category">
+                  <div class="emojis">
+                    <span
+                        v-for="(emoji, emojiName) in emojiGroup"
+                        :key="emojiName"
+                        @click="insertEmoji(emoji)"
+                        :title="emojiName"
+                    >{{ emoji }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </emoji-picker>
+        </span>
+
+        <!-- 文件 -->
         <span>文件、</span>
+
+        <!-- 发送按钮 -->
         <span @click="sendMsg()">发送</span>
       </div>
     </el-col>
@@ -54,9 +95,10 @@
 </template>
 
 <script>
-import Emoji from "@/component/window/Emoji";
 import chatMsgApi from "@/api/chatMsg";
 import EventBus from "@/component/event-bus";
+import EmojiPicker from "vue-emoji-picker";
+
 
 export default {
   name: "Window",
@@ -67,7 +109,7 @@ export default {
   ],
 
   components: {
-    Emoji
+    EmojiPicker
   },
 
   created() {
@@ -174,7 +216,14 @@ export default {
           divCls.scrollTop = divCls.scrollHeight
         }
       }, 0)
-    }
+    },
+
+    /**
+     * 点击Emoji
+     */
+    insertEmoji(emoji) {
+      this.chatMsg.content += emoji
+    },
   },
 }
 </script>
@@ -281,5 +330,63 @@ export default {
 .session-name-cls {
   color: $session-nick-color;
   font-family: "Arial Rounded MT Bold";
+}
+
+.wrapper {
+  display: inline-block;
+}
+
+.emoji-invoker {
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.2s;
+  padding: 0;
+  background: transparent;
+  border: 0;
+}
+
+.emoji-invoker:hover {
+  transform: scale(1.1);
+}
+
+.emoji-invoker > svg {
+  fill: #b1c6d0;
+}
+
+.emoji-picker {
+  border: 1px solid #ccc;
+  width: 30vw;
+  height: 50vh;
+  overflow: scroll;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  box-shadow: 1px 1px 8px #c7dbe6;
+  position: fixed;
+  transform: translate(-105%, -105%);
+}
+
+.emoji-picker .emojis {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.emoji-picker .emojis:after {
+  content: "";
+  flex: auto;
+}
+
+.emoji-picker .emojis span {
+  padding: 0.2rem;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.emoji-picker .emojis span:hover {
+  background: #ececec;
+  cursor: pointer;
 }
 </style>
