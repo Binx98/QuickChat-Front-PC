a1ad8ba2b6fb39b6d41150e9c99da79d8dadb5aa
徐志斌 <binx19980707@gmail.com>
2024-06-30 15:58:14 +0800

    前端样式开发

--- src/component/leftMenu/LeftMenu.vue
+++ src/component/leftMenu/LeftMenu.vue
@@ -45,7 +45,7 @@ export default {
 
 <style lang="scss" scoped>
 .div-cls {
-  height: 91vh;
+  height: 92vh;
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
   background-color: $window-item-color;
