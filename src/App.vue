85e31f98e3a3676e84348f8a482f32ee13d9db63
徐志斌 <binx19980707@gmail.com>
2024-07-07 15:35:04 +0800

    禁用浏览器页面默认右键菜单栏

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <!--  背景视频  -->
     <video src="@/assets/video/background.mp4" autoplay muted loop
            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;object-fit: cover;"/>
-    <!--  路由页面  -->
     <router-view/>
   </div>
 </template>
+
+<script>
+// 禁用浏览器页面默认右键菜单栏
+function banMenu() {
+  return false;
+}
+
+document.oncontextmenu = banMenu;
+</script>
 
 <style>
 /* 清除浏览器默认间距 */
